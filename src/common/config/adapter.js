175c5c0e45b443e41883d6e0fd9aa96474d2e08f
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-17 17:50:42 +0800

    Add: mysql db create

--- src/common/config/adapter.js
+++ src/common/config/adapter.js
@@ -42,8 +42,8 @@ exports.model = {
     encoding: 'utf8',
     host: '127.0.0.1',
     port: 3306,
-    user: 'root',
-    password: 'root',
+    user: 'nidemall',
+    password: '88888888',
     dateStrings: true
   },
   sqlite: {
