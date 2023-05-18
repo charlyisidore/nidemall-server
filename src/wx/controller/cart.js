193bda6db81a673591cc41f4e1f6a17b14595def
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-18 18:24:28 +0800

    Add: empty wx/cart controller

---
+++ src/wx/controller/cart.js
@@ -0,0 +1,35 @@
+const Base = require('./base.js');
+
+module.exports = class extends Base {
+  async indexAction() {
+    return this.success('todo');
+  }
+
+  async addAction() {
+    return this.success('todo');
+  }
+
+  async fastaddAction() {
+    return this.success('todo');
+  }
+
+  async updateAction() {
+    return this.success('todo');
+  }
+
+  async deleteAction() {
+    return this.success('todo');
+  }
+
+  async checkedAction() {
+    return this.success('todo');
+  }
+
+  async goodscountAction() {
+    return this.success('todo');
+  }
+
+  async checkoutAction() {
+    return this.success('todo');
+  }
+};
