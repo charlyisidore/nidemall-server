79e1aac59ebaad0bf8112fcc67b3af0b7a83c384
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-18 19:28:33 +0800

    Add: empty auth controller

---
+++ src/wx/controller/auth.js
@@ -0,0 +1,31 @@
+const Base = require('./base.js');
+
+module.exports = class extends Base {
+  async loginAction() {
+    return this.success('todo');
+  }
+
+  async login_by_weixinAction() {
+    return this.success('todo');
+  }
+
+  async regCaptchaAction() {
+    return this.success('todo');
+  }
+
+  async registerAction() {
+    return this.success('todo');
+  }
+
+  async resetAction() {
+    return this.success('todo');
+  }
+
+  async bindPhoneAction() {
+    return this.success('todo');
+  }
+
+  async logoutAction() {
+    return this.success('todo');
+  }
+};
