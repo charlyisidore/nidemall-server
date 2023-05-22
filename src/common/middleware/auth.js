5bf28dbe2377297412f8fb72a29ec625049285f3
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-22 16:58:44 +0800

    Fix: userId was not set properly in middleware

--- src/common/middleware/auth.js
+++ src/common/middleware/auth.js
@@ -2,11 +2,11 @@ module.exports = (options, app) => {
     const config = app.think.config('auth');
     const service = app.think.service('auth');
 
-    return (ctx, next) => {
+    return async (ctx, next) => {
         const token = ctx.header[config.header.toLowerCase()];
         if (token) {
             try {
-                const data = service.verifyToken(token);
+                const data = await service.verifyToken(token);
                 ctx.state.userId = data?.userId;
             } catch (e) { }
         }
