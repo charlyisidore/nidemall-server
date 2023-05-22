65a1d58f772185b7da3c8bba132a71f63dc03af0
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-22 16:35:16 +0800

    Fix: use app instead of global variable think in middleware, as recommended

--- src/common/middleware/auth.js
+++ src/common/middleware/auth.js
@@ -1,6 +1,6 @@
-module.exports = () => {
-    const config = think.config('auth');
-    const service = think.service('auth');
+module.exports = (options, app) => {
+    const config = app.think.config('auth');
+    const service = app.think.service('auth');
 
     return (ctx, next) => {
         const token = ctx.header[config.header.toLowerCase()];
