f369cb896988bf55b68f6879cfd1294144b7748c
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-24 21:18:53 +0800

    Add: services type definitions in @typedef jsdoc for Intellisense

---
+++ src/common/types.js
@@ -0,0 +1,25 @@
+/**
+ * @typedef {import('./service/ad.js')} AdService
+ * @typedef {import('./service/address.js')} AddressService
+ * @typedef {import('./service/auth.js')} AuthService
+ * @typedef {import('./service/brand.js')} BrandService
+ * @typedef {import('./service/cart.js')} CartService
+ * @typedef {import('./service/category.js')} CategoryService
+ * @typedef {import('./service/collect.js')} CollectService
+ * @typedef {import('./service/comment.js')} CommentService
+ * @typedef {import('./service/coupon.js')} CouponService
+ * @typedef {import('./service/coupon_user.js')} CouponUserService
+ * @typedef {import('./service/footprint.js')} FootprintService
+ * @typedef {import('./service/goods.js')} GoodsService
+ * @typedef {import('./service/goods_attribute.js')} GoodsAttributeService
+ * @typedef {import('./service/goods_product.js')} GoodsProductService
+ * @typedef {import('./service/goods_specification.js')} GoodsSpecificationService
+ * @typedef {import('./service/groupon_rules.js')} GrouponRulesService
+ * @typedef {import('./service/issue.js')} IssueService
+ * @typedef {import('./service/order.js')} OrderService
+ * @typedef {import('./service/search_history.js')} SearchHistoryService
+ * @typedef {import('./service/system.js')} SystemService
+ * @typedef {import('./service/topic.js')} TopicService
+ * @typedef {import('./service/user.js')} UserService
+ * @typedef {import('./service/weixin.js')} WeixinService
+ */
