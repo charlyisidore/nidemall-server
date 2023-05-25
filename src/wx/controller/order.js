827f59bc39b4aaf950456e54c71d576461cd06af
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-25 21:25:39 +0800

    Add: detail and submit actions in /wx/order controller

--- src/wx/controller/order.js
+++ src/wx/controller/order.js
@@ -1,16 +1,466 @@
 const Base = require('./base.js');
+const CouponUserService = require('../../common/service/coupon_user.js');
+const GrouponService = require('../../common/service/groupon.js');
+const GrouponRulesService = require('../../common/service/groupon_rules.js');
 
 module.exports = class WxOrderController extends Base {
+  static GROUPON = {
+    EXPIRED: 730,
+    OFFLINE: 731,
+    FULL: 732,
+    JOIN: 733,
+  };
+
+  static ORDER = {
+    UNKNOWN: 720,
+    INVALID: 721,
+    CHECKOUT_FAIL: 722,
+    CANCEL_FAIL: 723,
+    PAY_FAIL: 724,
+    INVALID_OPERATION: 725,
+    COMMENTED: 726,
+    COMMENT_EXPIRED: 727,
+  };
+
+  static STATUS = {
+    CREATE: 101,
+    CANCEL: 102,
+    AUTO_CANCEL: 103,
+    ADMIN_CANCEL: 104,
+    PAY: 201,
+    REFUND: 202,
+    REFUND_CONFIRM: 203,
+    SHIP: 301,
+    CONFIRM: 401,
+    AUTO_CONFIRM: 402,
+  };
+
   async listAction() {
-    return this.success('todo');
+    const userId = this.getUserId();
+    /** @type {number} */
+    const showType = this.get('showType');
+    /** @type {number} */
+    const page = this.get('page');
+    /** @type {number} */
+    const limit = this.get('limit');
+    /** @type {string} */
+    const sort = think.camelCase(this.get('sort'));
+    /** @type {string} */
+    const order = this.get('order');
+
+    /** @type {GrouponService} */
+    const grouponService = this.service('groupon');
+    /** @type {OrderService} */
+    const orderService = this.service('order');
+    /** @type {OrderGoodsService} */
+    const orderGoodsService = this.service('order_goods');
+
+    if (think.isNullOrUndefined(userId)) {
+      return this.unlogin();
+    }
+
+    const orderStatus = this.orderStatus(showType);
+    const orderList = await orderService.queryByOrderStatus(
+      userId,
+      orderStatus,
+      page,
+      limit,
+      sort,
+      order
+    );
+
+    const orderVoList = await Promise.all(
+      orderList.data.map(async (order) => {
+        const groupon = await grouponService.queryByOrderId(order.id);
+        const orderGoodsList = await orderGoodsService.queryByOid(order.id);
+
+        return {
+          id: order.id,
+          orderSn: order.orderSn,
+          actualPrice: order.actualPrice,
+          orderStatusText: this.orderStatusText(order),
+          handleOption: this.build(order),
+          aftersaleStatus: order.aftersaleStatus,
+          isGroupin: !think.isEmpty(groupon),
+          goodsList: orderGoodsList.map((orderGoods) => ({
+            id: orderGoods.id,
+            goodsName: orderGoods.goodsName,
+            number: orderGoods.number,
+            picUrl: orderGoods.picUrl,
+            specifications: orderGoods.specifications,
+            price: orderGoods.price,
+          })),
+        };
+      })
+    );
+
+    return this.successList(orderVoList, orderList);
   }
 
   async detailAction() {
-    return this.success('todo');
+    const userId = this.getUserId();
+    /** @type {number} */
+    const orderId = this.get('orderId');
+
+    /** @type {ExpressService} */
+    const expressService = this.service('express');
+    /** @type {OrderService} */
+    const orderService = this.service('order');
+    /** @type {OrderGoodsService} */
+    const orderGoodsService = this.service('order_goods');
+
+    const { ORDER, STATUS } = this.constructor;
+
+    if (think.isNullOrUndefined(userId)) {
+      return this.unlogin();
+    }
+
+    const order = await orderService.findById(orderId, userId);
+
+    if (think.isEmpty(order)) {
+      return this.fail(ORDER.UNKNOWN, '订单不存在');
+    }
+
+    if (order.userId != userId) {
+      return this.fail(ORDER.INVALID, '不是当前用户的订单');
+    }
+
+    const orderVo = {
+      id: order.id,
+      orderSn: order.orderSn,
+      message: order.message,
+      addTime: order.addTime,
+      consignee: order.consignee,
+      mobile: order.mobile,
+      address: order.address,
+      goodsPrice: order.goodsPrice,
+      couponPrice: order.couponPrice,
+      freightPrice: order.freightPrice,
+      actualPrice: order.actualPrice,
+      orderStatusText: this.orderStatusText(order),
+      handleOption: this.build(order),
+      aftersaleStatus: order.aftersaleStatus,
+      expCode: order.shipChannel,
+      expName: expressService.getVendorName(order.shipChannel),
+      expNo: order.shipSn,
+    };
+
+    const orderGoodsList = await orderGoodsService.queryByOid(order.id);
+
+    const result = {
+      orderInfo: orderVo,
+      orderGoods: orderGoodsList,
+      expressInfo: {},
+    };
+
+    if (STATUS.SHIP === order.orderStatus) {
+      const expressInfo = await expressService.getExpressInfo(order.shipChannel, order.shipSn);
+
+      if (!think.isEmpty(expressInfo)) {
+        Object.assign(result, {
+          expressInfo,
+        });
+      }
+    }
+
+    return this.success(result);
   }
 
   async submitAction() {
-    return this.success('todo');
+    const userId = this.getUserId();
+    /** @type {number} */
+    const cartId = this.get('cartId');
+    /** @type {number} */
+    const addressId = this.get('addressId');
+    /** @type {number} */
+    const couponId = this.get('couponId');
+    /** @type {number?} */
+    const userCouponId = this.get('userCouponId');
+    /** @type {string?} */
+    const message = this.get('message');
+    /** @type {number?} */
+    const grouponRulesId = this.get('grouponRulesId');
+    /** @type {number?} */
+    const grouponLinkId = this.get('grouponLinkId');
+
+    /** @type {AddressService} */
+    const addressService = this.service('address');
+    /** @type {CartService} */
+    const cartService = this.service('cart');
+    /** @type {CouponService} */
+    const couponService = this.service('coupon');
+    /** @type {CouponUserService} */
+    const couponUserService = this.service('coupon_user');
+    /** @type {GoodsProductService} */
+    const goodsProductService = this.service('goods_product');
+    /** @type {GrouponService} */
+    const grouponService = this.service('groupon');
+    /** @type {GrouponRulesService} */
+    const grouponRulesService = this.service('groupon_rules');
+    /** @type {OrderService} */
+    const orderService = this.service('order');
+    /** @type {OrderGoodsService} */
+    const orderGoodsService = this.service('order_goods');
+    /** @type {SystemService} */
+    const systemService = this.service('system');
+
+    const { GROUPON, STATUS } = this.constructor;
+    const { RULE_STATUS } = GrouponRulesService;
+
+    const freight = await systemService.getFreight();
+    const freightLimit = await systemService.getFreightLimit();
+    const now = new Date();
+
+    if (think.isNullOrUndefined(userId)) {
+      return this.unlogin();
+    }
+
+    if (!think.isEmpty(grouponRulesId)) {
+      const rules = await grouponRulesService.findById(grouponRulesId);
+
+      if (think.isEmpty(rules)) {
+        return this.badArgument();
+      }
+
+      if (RULE_STATUS.DOWN_EXPIRE == rules.status) {
+        return this.fail(GROUPON.EXPIRED, '团购已过期!');
+      }
+
+      if (RULE_STATUS.DOWN_ADMIN == rules.status) {
+        return this.fail(GROUPON.OFFLINE, '团购已下线!');
+      }
+
+      if (!think.isEmpty(grouponLinkId)) {
+        if ((await grouponService.countGroupon(grouponLinkId)) > (rules.discountMember - 1)) {
+          return this.fail(GROUPON.FULL, '团购活动人数已满!');
+        }
+
+        if (await grouponService.hasJoin(grouponLinkId, userId)) {
+          return this.fail(GROUPON.JOIN, '团购活动已经参加!');
+        }
+
+        const groupon = await grouponService.queryById(grouponLinkId, userId);
+
+        if (!think.isEmpty(groupon) && groupon.creatorUserId == userId) {
+          return this.fail(GROUPON.JOIN, '团购活动已经参加!');
+        }
+      }
+    }
+
+    const checkedAddres = await addressService.query(addressId, userId);
+
+    if (think.isEmpty(checkedAddres)) {
+      return this.badArgument();
+    }
+
+    let grouponPrice = 0.;
+    const grouponRules = await grouponRulesService.findById(grouponRulesId);
+
+    if (!think.isEmpty(grouponRules)) {
+      grouponPrice = grouponRules.discount;
+    }
+
+    let checkedGoodsList = null;
+
+    if (!cartId) {
+      checkedGoodsList = await cartService.queryByUidAndChecked(userId);
+    } else {
+      const cart = await cartService.findById(cartId);
+      checkedGoodsList = [cart];
+    }
+
+    if (checkedGoodsList.length == 0) {
+      return this.badArgumentValue();
+    }
+
+    let checkedGoodsPrice = 0.;
+    for (const checkGoods of checkedGoodsList) {
+      if (!think.isEmpty(grouponRules) && grouponRules.goodsId == checkGoods.goodsId) {
+        checkedGoodsPrice += (checkGoods.price - grouponPrice) * checkGoods.number;
+      } else {
+        checkedGoodsList += checkGoods.price * checkGoods.number;
+      }
+    }
+
+    let couponPrice = 0.;
+    if (couponId && couponId != -1) {
+      const coupon = await couponService.checkCoupon(
+        userId,
+        couponId,
+        userCouponId,
+        checkedGoodsPrice,
+        checkedGoodsList
+      );
+
+      if (think.isEmpty(coupon)) {
+        return this.badArgumentValue();
+      }
+
+      couponPrice = coupon.discount;
+    }
+
+    let freightPrice = 0.;
+    if (checkedGoodsPrice < freightLimit) {
+      freightPrice = freight;
+    }
+
+    const integralPrice = 0.;
+    const orderTotalPrice = Math.max(0., checkedGoodsPrice + freightPrice - couponPrice);
+    const actualPrice = orderTotalPrice - integralPrice;
+
+    const order = {
+      userId,
+      orderSn: await orderService.generateOrderSn(userId),
+      orderStatus: STATUS.CREATE,
+      consignee: checkedAddres.name,
+      mobile: checkedAddres.tel,
+      message,
+      address: `${checkedAddres.province}${checkedAddres.city}${checkedAddres.county} ${checkedAddres.addressDetail}`,
+      goodsPrice: checkedGoodsPrice,
+      freightPrice,
+      couponPrice,
+      integralPrice,
+      orderPrice: orderTotalPrice,
+      actualPrice,
+      grouponPrice: think.isEmpty(grouponRules) ? 0. : grouponPrice,
+    };
+
+    order.id = await orderService.add(order);
+
+    for (const cartGoods of checkedGoodsList) {
+      const orderGoods = {
+        orderId: order.id,
+        goodsId: cartGoods.goodsId,
+        goodsSn: cartGoods.goodsSn,
+        productId: cartGoods.productId,
+        goodsName: cartGoods.goodsName,
+        picUrl: cartGoods.picUrl,
+        price: cartGoods.price,
+        number: cartGoods.number,
+        specifications: cartGoods.specifications,
+        addTime: now,
+      };
+
+      await orderGoodsService.add(orderGoods);
+    }
+
+    if (cartId) {
+      await cartService.deleteById(cartId);
+    } else {
+      await cartService.clearGoods(userId);
+    }
+
+    for (const checkGoods of checkedGoodsList) {
+      const productId = checkGoods.productId;
+      const product = await goodsProductService.findById(productId);
+
+      const remainNumber = product.number - checkGoods.number;
+
+      if (remainNumber < 0) {
+        throw new Error('下单的商品货品数量大于库存量');
+      }
+
+      if (!await goodsProductService.reduceStock(productId, checkGoods.number)) {
+        throw new Error('商品货品库存减少失败');
+      }
+    }
+
+    if (couponId && couponId != -1) {
+      const couponUser = await couponUserService.findById(userCouponId);
+
+      Object.assign(couponUser, {
+        status: CouponUserService.STATUS.USED,
+        usedTime: now,
+        orderId: order.id,
+      });
+
+      await couponUserService.update(couponUser);
+    }
+
+    if (grouponRulesId) {
+      const groupon = {
+        orderId: order.id,
+        status: GrouponService.STATUS.NONE,
+        userId,
+        rulesId: grouponRulesId,
+      };
+
+      if (grouponLinkId) {
+        const baseGroupon = await grouponService.queryById(grouponLinkId);
+
+        Object.assign(groupon, {
+          creatorUserId: baseGroupon.creatorUserId,
+          grouponId: grouponLinkId,
+          shareUrl: baseGroupon.shareUrl,
+        });
+
+        await grouponService.createGroupon(groupon);
+      } else {
+        Object.assign(groupon, {
+          creatorUserId: userId,
+          creatorUserTime: now,
+          grouponId: 0,
+        });
+
+        grouponLinkId = await grouponService.createGroupon(groupon);
+      }
+    }
+
+    let payed = false;
+
+    // Deal with float precision
+    if (order.actualPrice < 0.001) {
+      payed = true;
+
+      await orderService.updateSelective({
+        id: order.id,
+        orderStatus: STATUS.PAY,
+      });
+
+      const groupon = await grouponService.queryByOrderId(order.id);
+
+      if (!think.isEmpty(groupon)) {
+        const grouponRules = await grouponRulesService.findById(groupon.rulesId);
+
+        if (groupon.grouponId == 0) {
+          // TODO
+          groupon.shareUrl = 'TODO';//qrCodeService.createGrouponShareImage(grouponRules.goodsName, grouponRules.picUrl, groupon);
+        }
+
+        groupon.status = GrouponService.STATUS.ON;
+
+        if (!await grouponService.updateById(groupon)) {
+          throw new Error('更新数据已失效');
+        }
+
+        const grouponList = await grouponService.queryJoinRecord(groupon.grouponId);
+
+        if (groupon.groupondId && grouponList.length >= (grouponRules.discountMember - 1)) {
+          for (const grouponActivity of grouponList) {
+            grouponActivity.status = GrouponService.STATUS.SUCCEED;
+
+            await grouponService.updateById(grouponActivity);
+          }
+
+          const grouponSource = await grouponService.queryById(groupon.grouponId);
+          grouponSource.status = GrouponService.STATUS.SUCCEED;
+          await grouponService.updateById(grouponSource);
+        }
+      }
+
+      // TODO
+      // await notifyService.notifyMail('新订单通知', order.toString());
+      // await notifyService.notifySmsTemplateSync(order.mobile, NotifyService.PAY.SUCCEED, order.orderSn.substr(8, 14));
+    } else {
+      // TODO
+      // await taskService.addTask(new OrderUnpaidTask(order.id));
+    }
+
+    return this.success({
+      orderId: order.id,
+      payed,
+      grouponLinkId: (grouponRulesId ? grouponLinkId : 0),
+    });
   }
 
   async cancelAction() {
@@ -39,5 +489,138 @@ module.exports = class WxOrderController extends Base {
 
   async commentAction() {
     return this.success('todo');
+  }
+
+  /**
+   * 
+   * @param {number} showType 
+   * @returns {number[]?}
+   */
+  orderStatus(showType) {
+    switch (showType) {
+      case 0:
+        // Full order
+        return null;
+      case 1:
+        // Orders to be paid
+        return [101];
+      case 2:
+        // Orders to be shipped
+        return [201];
+      case 3:
+        // Orders to be received
+        return [301];
+      case 4:
+        // Orders to be evaluated
+        return [401];
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * 
+   * @param {{ orderStatus: number }} order 
+   * @returns {string}
+   */
+  orderStatusText(order) {
+    switch (order.orderStatus) {
+      case 101:
+        // Not paid
+        return '未付款';
+      case 102:
+        // Canceled
+        return '已取消';
+      case 103:
+        // Canceled (system)
+        return '已取消(系统)';
+      case 201:
+        // Paid
+        return '已付款';
+      case 202:
+        // Order cancelled, refund in progress
+        return '订单取消，退款中';
+      case 203:
+        // Refunded
+        return '已退款';
+      case 204:
+        // Timed out group buy
+        return '已超时团购';
+      case 301:
+        // Shipped
+        return '已发货';
+      case 401:
+        // Received
+        return '已收货';
+      case 402:
+        // Received (system)
+        return '已收货(系统)';
+      default:
+        throw new Error('orderStatus不支持');
+    }
+  }
+
+  /**
+   * 
+   * @param {{ orderStatus: number }} order 
+   * @returns {{ cancel: boolean?, pay: boolean?, delete: boolean?, refund: boolean?, confirm: boolean?, comment: boolean?, rebuy: boolean?, aftersale: boolean? }}
+   */
+  build(order) {
+    const handleOption = {};
+
+    switch (order.orderStatus) {
+      case 101:
+        // If the order is not cancelled and not paid for, it is payable and can be cancelled
+        Object.assign(handleOption, {
+          cancel: true,
+          pay: true,
+        });
+        break;
+      case 102:
+      case 103:
+        // If the order has been cancelled or completed, it can be deleted
+        Object.assign(handleOption, {
+          delete: true,
+        });
+        break;
+      case 201:
+        // If an order is paid for and not shipped, a refund will be issued
+        Object.assign(handleOption, {
+          refund: true,
+        });
+        break;
+      case 202:
+      case 204:
+        // If the order request for refund is in progress, there is no relevant operation
+        break;
+      case 203:
+        // If the order has been refunded, it can be deleted
+        Object.assign(handleOption, {
+          delete: true,
+        });
+        break;
+      case 301:
+        // If the order has been shipped and not received, then you can receive the goods operation,
+        // at this time can not cancel the order
+        Object.assign(handleOption, {
+          confirm: true,
+        });
+        break;
+      case 401:
+      case 402:
+        // If an order has been paid for and received, it can be deleted, de-commented,
+        // requested after-sales and purchased again
+        Object.assign(handleOption, {
+          delete: true,
+          comment: true,
+          rebuy: true,
+          aftersale: true,
+        });
+        break;
+      default:
+        throw new Error('status不支持');
+    }
+
+    return handleOption;
   }
 };
--- src/wx/logic/order.js
+++ src/wx/logic/order.js
@@ -1,14 +1,71 @@
 module.exports = class extends think.Logic {
   listAction() {
     this.allowMethods = 'GET';
+
+    this.rules = {
+      showType: {
+        int: true,
+        default: 0,
+      },
+      page: {
+        int: true,
+        default: 1,
+      },
+      limit: {
+        int: true,
+        default: 10,
+      },
+      sort: {
+        string: true,
+        default: 'add_time',
+      },
+      order: {
+        string: true,
+        default: 'desc',
+      },
+    };
   }
 
   detailAction() {
     this.allowMethods = 'GET';
+
+    this.rules = {
+      orderId: {
+        int: true,
+        required: true,
+      },
+    };
   }
 
   submitAction() {
     this.allowMethods = 'POST';
+
+    this.rules = {
+      cartId: {
+        int: true,
+        required: true,
+      },
+      addressId: {
+        int: true,
+        required: true,
+      },
+      couponId: {
+        int: true,
+        required: true,
+      },
+      userCouponId: {
+        int: true,
+      },
+      message: {
+        string: true,
+      },
+      grouponRulesId: {
+        int: true,
+      },
+      grouponLinkId: {
+        int: true,
+      },
+    };
   }
 
   cancelAction() {
