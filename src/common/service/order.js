b71c09e31ad7dbb6f5037ce8f5c547b8dc0f7cdb
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-25 21:07:16 +0800

    Add: numerous methods in OrderService for /wx/order controller

--- src/common/service/order.js
+++ src/common/service/order.js
@@ -18,12 +18,137 @@ module.exports = class OrderService extends think.Service {
 
   /**
    * 
+   * @param {Order} order 
+   * @returns {Promise<number>} The ID inserted
+   */
+  add(order) {
+    const now = new Date();
+    return this.model('order')
+      .add(Object.assign(order, {
+        addTime: now,
+        updateTime: now,
+      }));
+  }
+
+  /**
+   * 
+   * @param {number} id 
+   * @param {number} userId 
+   * @returns {Promise<Order|Record<string, never>>}
+   */
+  findById(id, userId) {
+    const where = { id };
+
+    if (!think.isNullOrUndefined(userId)) {
+      Object.assign(where, {
+        userId,
+        deleted: false,
+      })
+    }
+
+    return this.model('order')
+      .where(where)
+      .find();
+  }
+
+  /**
+   * 
+   * @param {number} orderSn 
+   * @param {number} userId 
+   */
+  countByOrderSn(orderSn, userId) {
+    return this.model('order')
+      .where({
+        orderSn,
+        userId,
+        deleted: false,
+      })
+      .count();
+  }
+
+  /**
+   * 
+   * @param {number} userId 
+   * @returns {string} A random orderSn
+   */
+  async generateOrderSn(userId) {
+    const now = new Date();
+    const yyyy = now.getFullYear().toString().padStart(4, '0');
+    const mm = now.getMonth().toString().padStart(2, '0');
+    const dd = now.getDate().toString().padStart(2, '0');
+    const date = `${yyyy}${mm}${dd}`;
+
+    let orderSn = `${date}${this.getRandomNum(6)}`;
+    while (await this.countByOrderSn(orderSn, userId)) {
+      orderSn = `${date}${this.getRandomNum(6)}`;
+    }
+
+    return orderSn;
+  }
+
+  /**
+   * 
+   * @param {number} userId 
+   * @param {number[]?} orderStatus 
+   * @param {number} page 
+   * @param {number} limit 
+   * @param {string} sort 
+   * @param {string} order 
+   * @returns {Promise<{pageSize: number, currentPage: number, count: number, totalPages: number, data: Order[]}>}
+   */
+  queryByOrderStatus(userId, orderStatus, page, limit, sort, order) {
+    const model = this.model('order');
+    const where = {
+      userId,
+      deleted: false,
+    };
+
+    if (!think.isEmpty(orderStatus)) {
+      Object.assign(where, {
+        orderStatus: ['IN', orderStatus],
+      });
+    }
+
+    if (!think.isNullOrUndefined(sort) && !think.isNullOrUndefined(order)) {
+      model.order({
+        [sort]: order,
+      });
+    } else {
+      model.order({
+        'addTime': 'DESC',
+      });
+    }
+
+    return model
+      .where(where)
+      .page(page, limit)
+      .countSelect();
+  }
+
+  /**
+   * 
+   * @param {Order} order 
+   * @returns {Promise<number>} The number of rows affected
+   */
+  updateSelective(order) {
+    return this.model('order')
+      .where({
+        id: order.id,
+      })
+      .update(order);
+  }
+
+  /**
+   * 
    * @param {number} userId 
    * @returns {{unpaid: number, unship: number, unrecv: number, uncomment: number}}
    */
   async orderInfo(userId) {
     const orders = await this.model('order')
-      .field('orderStatus,comments')
+      .field([
+        'orderStatus',
+        'comments',
+      ].join(','))
       .where({
         userId,
         deleted: false,
@@ -58,6 +183,12 @@ module.exports = class OrderService extends think.Service {
       unrecv,
       uncomment,
     };
+  }
+
+  getRandomNum(n) {
+    return Math.floor(Math.random() * (Math.pow(10, n) - 1))
+      .toString()
+      .padStart(n, '0');
   }
 
   isCreateStatus(order) {
