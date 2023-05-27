085997cebc2b7bf8c62bd907cab3e71086d20bfa
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-28 07:50:22 +0800

    Add: unit test for /wx/brand

--- test/wx/controller/brand.js
+++ test/wx/controller/brand.js
@@ -3,6 +3,19 @@ const request = require('supertest');
 const path = require('path');
 require(path.join(process.cwd(), 'production.js'));
 
+const BRAND_ID = [
+  1001000, 1001002, 1001003, 1001007, 1001008,
+  1001010, 1001012, 1001013, 1001015, 1001016,
+  1001020, 1001037, 1001038, 1001039, 1001045,
+  1003000, 1005001, 1006000, 1008000, 1010001,
+  1010002, 1015000, 1016002, 1018000, 1021000,
+  1022000, 1022001, 1023000, 1024000, 1024001,
+  1024003, 1024006, 1025000, 1025001, 1026000,
+  1026001, 1028000, 1028003, 1033003, 1033004,
+  1034001, 1037000, 1038000, 1038001, 1039000,
+  1039001, 1040000, 1041000, 1046000
+];
+
 test.serial('list', async (t) => {
   const url = '/wx/brand/list';
   let pages = 0;
@@ -37,4 +50,47 @@ test.serial('list', async (t) => {
     t.is(actual.errno, expected.errno);
     t.deepEqual(actual.data, expected.data);
   }
+});
+
+test.serial('detail', async (t) => {
+  const url = '/wx/brand/detail';
+
+  // Missing `id`
+  {
+    const response = await request(think.app.listen())
+      .get(url)
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    t.is(response.body.errno, 402);
+  }
+
+  // Not found `id`
+  {
+    const response = await request(think.app.listen())
+      .get(url)
+      .query({ id: 99999999 })
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    t.is(response.body.errno, 402);
+  }
+
+  // With `id`
+  await Promise.all(
+    BRAND_ID
+      .map(async (id) => {
+        const response = await request(think.app.listen())
+          .get(url)
+          .query({ id })
+          .expect('Content-Type', /json/)
+          .expect(200);
+
+        const actual = response.body;
+        const expected = require(`./brand/detail_${id}.json`);
+
+        t.is(actual.errno, expected.errno);
+        t.deepEqual(actual.data, expected.data);
+      })
+  );
 });
