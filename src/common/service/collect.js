ecaf45d07cd429eba6cb60bdbb0cedecc640c540
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-29 18:53:29 +0800

    Add: finished /wx/collect controller

--- src/common/service/collect.js
+++ src/common/service/collect.js
@@ -20,4 +20,70 @@ module.exports = class CollectService extends think.Service {
       })
       .count();
   }
+
+  /**
+   * 
+   * @param {number} userId 
+   * @param {number?} type 
+   * @param {number} page 
+   * @param {number} limit 
+   * @param {string?} sort 
+   * @param {string?} order 
+   * @returns {Promise<{pageSize: number, currentPage: number, count: number, totalPages: number, data: Collect[]}>}
+   */
+  queryByType(userId, type, page, limit, sort, order) {
+    const model = this.model('collect');
+
+    const where = {
+      userId,
+      deleted: false,
+    };
+
+    if (!think.isNullOrUndefined(type)) {
+      Object.assign(where, { type });
+    }
+
+    model.where(where);
+
+    if (!think.isNullOrUndefined(sort) && !think.isNullOrUndefined(order)) {
+      model.order({
+        [sort]: order,
+      });
+    }
+
+    return model
+      .page(page, limit)
+      .countSelect();
+  }
+
+  /**
+   * 
+   * @param {number} userId 
+   * @param {number} type 
+   * @param {number} valueId 
+   * @returns {Promise<Collect>}
+   */
+  queryByTypeAndValue(userId, type, valueId) {
+    return this.model('collect')
+      .where({
+        userId,
+        type,
+        valueId,
+        deleted: false,
+      })
+      .find();
+  }
+
+  /**
+   * 
+   * @param {number} id 
+   * @returns {Promise<number>} The number of rows affected
+   */
+  deleteById(id) {
+    return this.model('collect')
+      .where({ id })
+      .update({
+        deleted: true,
+      });
+  }
 }
--- src/wx/controller/collect.js
+++ src/wx/controller/collect.js
@@ -2,10 +2,92 @@ const Base = require('./base.js');
 
 module.exports = class WxCollectController extends Base {
   async listAction() {
-    return this.success('todo');
+    const userId = this.getUserId();
+    /** @type {number} */
+    const type = this.get('type');
+    /** @type {number} */
+    const page = this.get('page');
+    /** @type {number} */
+    const limit = this.get('limit');
+    /** @type {string} */
+    const sort = think.camelCase(this.get('sort'));
+    /** @type {string} */
+    const order = this.get('order');
+
+    /** @type {CollectService} */
+    const collectService = this.service('collect');
+    /** @type {GoodsService} */
+    const goodsService = this.service('goods');
+    /** @type {TopicService} */
+    const topicService = this.service('topic');
+
+    if (think.isNullOrUndefined(userId)) {
+      return this.unlogin();
+    }
+
+    const collectList = await collectService.queryByType(userId, type, page, limit, sort, order)
+
+    const collectVoList = await Promise.all(
+      collectList.data.map(async (collect) => {
+        const collectVo = {
+          id: collect.id,
+          type: collect.type,
+          valueId: collect.valueId,
+        };
+
+        if (0 == type) {
+          const goods = await goodsService.findById(collect.valueId);
+
+          Object.assign(collectVo, {
+            name: goods.name,
+            brief: goods.brief,
+            picUrl: goods.picUrl,
+            retailPrice: goods.retailPrice,
+          });
+        } else {
+          const topic = await topicService.findById(collect.valueId);
+
+          Object.assign(collectVo, {
+            name: topic.title,
+            subtitle: topic.title,
+            price: topic.price,
+            picUrl: topic.picUrl,
+          });
+        }
+
+        return collectVo;
+      })
+    );
+
+    return this.successList(collectVoList, collectList);
   }
 
   async addordeleteAction() {
-    return this.success('todo');
+    const userId = this.getUserId();
+    /** @type {number} */
+    const type = this.get('type');
+    /** @type {number} */
+    const valueId = this.get('valueId');
+
+    /** @type {CollectService} */
+    const collectService = this.service('collect');
+
+    if (think.isNullOrUndefined(userId)) {
+      return this.unlogin();
+    }
+
+    const collect = await collectService.queryByTypeAndValue(userId, type, valueId);
+
+    if (think.isEmpty(collect)) {
+      await collectService.add({
+        userId,
+        type,
+        valueId,
+      });
+    } else {
+      await collectService.deleteById(collect.id);
+    }
+
+    return this.success();
   }
 };
