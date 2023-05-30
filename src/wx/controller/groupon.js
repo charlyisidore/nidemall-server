87dcbf31a9c9f4ffe156cf753bb199fa94477abf
Charly Lersteau <charly.lersteau@gmx.fr>
2023-05-30 11:26:13 +0800

    Add: /wx/controller/groupon action

--- src/wx/controller/groupon.js
+++ src/wx/controller/groupon.js
@@ -2,7 +2,26 @@ const Base = require('./base.js');
 
 module.exports = class WxGrouponController extends Base {
   async listAction() {
-    return this.success('todo');
+    const userId = this.getUserId();
+    /** @type {number} */
+    const page = this.get('page');
+    /** @type {number} */
+    const limit = this.get('limit');
+    /** @type {string} */
+    const sort = think.camelCase(this.get('sort'));
+    /** @type {string} */
+    const order = this.get('order');
+
+    /** @type {GrouponRulesService} */
+    const grouponRulesService = this.service('groupon_rules');
+
+    if (think.isNullOrUndefined(userId)) {
+      return this.unlogin();
+    }
+
+    const grouponRuleVoList = await grouponRulesService.wxQueryList(page, limit, sort, order);
+
+    return this.successList(grouponRuleVoList);
   }
 
   async detailAction() {
