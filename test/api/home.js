8c70589ccabd594f889219d8dcc9d8c2eaecdc09
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-02 20:51:02 +0800

    Add: 100% coverage in /wx/home

--- test/api/home.js
+++ test/api/home.js
@@ -16,3 +16,15 @@ test('/wx/home/index', async (t) => {
     think.omit(liteResponse.data, omitKeys)
   );
 });
+
+test('/wx/home/about', async (t) => {
+  const [nideResponse, liteResponse] = await dualRequest(t, {
+    path: '/wx/home/about',
+  })
+
+  t.is(nideResponse.errno, 0, '/about success');
+  t.is(nideResponse.errno, liteResponse.errno, '/about diff errno');
+
+  const omitKeys = ['grouponList'];
+  deepEqualType(t, nideResponse.data, liteResponse.data);
+});
