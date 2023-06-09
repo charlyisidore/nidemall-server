aa9269a57b03cef6faf012d32809b05672e12cb2
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-09 17:17:54 +0800

    Fix: wrong indent size

--- src/common/middleware/auth.js
+++ src/common/middleware/auth.js
@@ -1,16 +1,16 @@
 module.exports = (options, app) => {
-    const config = app.think.config('auth');
-    const service = app.think.service('auth');
+  const config = app.think.config('auth');
+  const service = app.think.service('auth');
 
-    return async (ctx, next) => {
-        const token = ctx.header[config.header.toLowerCase()];
-        if (token) {
-            try {
-                const data = await service.verifyToken(token);
-                ctx.state.userId = data?.userId;
-            } catch (e) { }
-        }
+  return async (ctx, next) => {
+    const token = ctx.header[config.header.toLowerCase()];
+    if (token) {
+      try {
+        const data = await service.verifyToken(token);
+        ctx.state.userId = data?.userId;
+      } catch (e) { }
+    }
 
-        return next();
-    };
+    return next();
+  };
 };
