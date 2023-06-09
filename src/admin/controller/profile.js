2f9a2390b1b7bff7acad79775e7d57e56bc5e4ae
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-09 21:52:50 +0800

    Add: /admin/profile nnotice and lsnotice actions

--- src/admin/controller/profile.js
+++ src/admin/controller/profile.js
@@ -6,11 +6,44 @@ module.exports = class AdminProfileController extends Base {
   }
 
   async nnoticeAction() {
-    return this.success('todo');
+    const adminId = this.getAdminId();
+    /** @type {NoticeAdminService} */
+    const noticeAdminService = this.service('notice_admin');
+
+    const count = await noticeAdminService.countUnread(adminId);
+
+    return this.success(count);
   }
 
   async lsnoticeAction() {
-    return this.success('todo');
+    const adminId = this.getAdminId();
+    /** @type {string?} */
+    const title = this.post('title');
+    /** @type {string?} */
+    const type = this.post('type');
+    /** @type {number} */
+    const page = this.get('page');
+    /** @type {number} */
+    const limit = this.get('limit');
+    /** @type {string} */
+    const sort = think.camelCase(this.get('sort'));
+    /** @type {string} */
+    const order = this.get('order');
+
+    /** @type {NoticeAdminService} */
+    const noticeAdminService = this.service('notice_admin');
+
+    const noticeList = await noticeAdminService.querySelective(
+      title,
+      type,
+      adminId,
+      page,
+      limit,
+      sort,
+      order
+    );
+
+    return this.successList(noticeList);
   }
 
   async catnoticeAction() {
--- src/admin/logic/profile.js
+++ src/admin/logic/profile.js
@@ -9,6 +9,33 @@ module.exports = class extends think.Logic {
 
   lsnoticeAction() {
     this.allowMethods = 'GET';
+
+    this.rules = {
+      title: {
+        string: true,
+      },
+      type: {
+        string: true,
+      },
+      page: {
+        int: true,
+        default: 1,
+      },
+      limit: {
+        int: true,
+        default: 10,
+      },
+      sort: {
+        string: true,
+        in: ['add_time', 'id'],
+        default: 'add_time',
+      },
+      order: {
+        string: true,
+        in: ['asc', 'desc'],
+        default: 'desc',
+      },
+    };
   }
 
   catnoticeAction() {
