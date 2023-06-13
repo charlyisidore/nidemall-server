6d120bb6286053eba9083a4cd9a727d8014a3dd0
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-13 14:49:01 +0800

    Add: empty /admin/order controller

---
+++ src/admin/controller/order.js
@@ -0,0 +1,35 @@
+const Base = require('./base.js');
+
+module.exports = class AdminOrderController extends Base {
+  async listAction() {
+    return this.success('todo');
+  }
+
+  async channelAction() {
+    return this.success('todo');
+  }
+
+  async detailAction() {
+    return this.success('todo');
+  }
+
+  async refundAction() {
+    return this.success('todo');
+  }
+
+  async shipAction() {
+    return this.success('todo');
+  }
+
+  async payAction() {
+    return this.success('todo');
+  }
+
+  async deleteAction() {
+    return this.success('todo');
+  }
+
+  async replyAction() {
+    return this.success('todo');
+  }
+};
---
+++ src/admin/logic/order.js
@@ -0,0 +1,75 @@
+module.exports = class extends think.Logic {
+  listAction() {
+    this.allowMethods = 'GET';
+
+    this.rules = {
+      nickname: {
+        string: true,
+      },
+      consignee: {
+        string: true,
+      },
+      orderSn: {
+        string: true,
+      },
+      start: {
+        date: true,
+      },
+      end: {
+        date: true,
+      },
+      orderStatusArray: {
+        array: true,
+        children: {
+          int: true,
+        },
+      },
+      page: {
+        int: true,
+        default: 1,
+      },
+      limit: {
+        int: true,
+        default: 10,
+      },
+      sort: {
+        string: true,
+        in: ['add_time', 'id'],
+        default: 'add_time',
+      },
+      order: {
+        string: true,
+        in: ['asc', 'desc'],
+        default: 'desc',
+      },
+    };
+  }
+
+  channelAction() {
+    this.allowMethods = 'GET';
+  }
+
+  detailAction() {
+    this.allowMethods = 'GET';
+  }
+
+  refundAction() {
+    this.allowMethods = 'POST';
+  }
+
+  shipAction() {
+    this.allowMethods = 'POST';
+  }
+
+  payAction() {
+    this.allowMethods = 'POST';
+  }
+
+  deleteAction() {
+    this.allowMethods = 'POST';
+  }
+
+  replyAction() {
+    this.allowMethods = 'POST';
+  }
+};
