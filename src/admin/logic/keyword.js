bdeb8b4b7ae6291c09e0b69d73a8b0cb90cdebac
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-13 11:04:57 +0800

    Add: empty /admin/keyword controller

---
+++ src/admin/logic/keyword.js
@@ -0,0 +1,72 @@
+module.exports = class extends think.Logic {
+  listAction() {
+    this.allowMethods = 'GET';
+
+    this.rules = {
+      keyword: {
+        string: true,
+      },
+      url: {
+        string: true,
+      },
+      page: {
+        int: true,
+        default: 1,
+      },
+      limit: {
+        int: true,
+        default: 10,
+      },
+      sort: {
+        string: true,
+        in: ['add_time', 'id'],
+        default: 'add_time',
+      },
+      order: {
+        string: true,
+        in: ['asc', 'desc'],
+        default: 'desc',
+      },
+    };
+  }
+
+  createAction() {
+    this.allowMethods = 'POST';
+
+    this.rules = {
+    };
+  }
+
+  readAction() {
+    this.allowMethods = 'GET';
+
+    this.rules = {
+      id: {
+        int: true,
+        required: true,
+      },
+    };
+  }
+
+  updateAction() {
+    this.allowMethods = 'POST';
+
+    this.rules = {
+      id: {
+        int: true,
+        required: true,
+      },
+    };
+  }
+
+  deleteAction() {
+    this.allowMethods = 'POST';
+
+    this.rules = {
+      id: {
+        int: true,
+        required: true,
+      },
+    };
+  }
+};
