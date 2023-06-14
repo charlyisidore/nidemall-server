fce3d6d92fe7b2a0dbbe6b4063c0258c10835649
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-14 21:07:16 +0800

    Add: DbService for database transactions

--- src/common/service/base.js
+++ src/common/service/base.js
@@ -1,5 +1,36 @@
+const EventEmitter = require('node:events');
+
+/**
+ * Base class for services.
+ */
 module.exports = class BaseService extends think.Service {
   constructor() {
     super();
+    this._eventEmitter = new EventEmitter();
+  }
+
+  /**
+   * Add an event listener.
+   * @param {string} eventName 
+   * @param {(...args: any[]) => void} listener 
+   * @returns {this}
+   */
+  on(eventName, listener) {
+    this._eventEmitter.on(eventName, listener);
+    return this;
+  }
+
+  /**
+   * Create a model and emit a `model` event.
+   * @see {DbService}
+   * @param {string} name 
+   * @param {any?} config 
+   * @param {string?} module 
+   * @returns {think.Model}
+   */
+  model(name, config, module) {
+    let model = super.model(name, config, module);
+    this._eventEmitter.emit('model', model);
+    return model;
   }
 }
---
+++ src/common/service/db.js
@@ -0,0 +1,110 @@
+const Base = require('./base.js');
+
+/**
+ * Helper service for database transactions.
+ */
+module.exports = class DbService extends Base {
+  /** Model name used for transactions */
+  static MODEL_NAME = 'ad';
+
+  /** Store models used for transactions */
+  _stack = [];
+
+  constructor() {
+    super();
+  }
+
+  /**
+   * Listen to `model` events emitted from services.
+   * @param {object[]} services 
+   */
+  listen(services) {
+    if (!think.isArray(services)) {
+      services = [services];
+    }
+    services.forEach(
+      (service) => service
+        .on('model', (model) => this._onModel(model))
+    );
+  }
+
+  /**
+   * Make a database transaction.
+   * @param {*} callback 
+   * @returns 
+   */
+  async transaction(callback) {
+    try {
+      await this._startTrans();
+      const result = await callback();
+      await this._commit();
+      return result;
+    } catch (e) {
+      await this._rollback();
+      throw e;
+    } finally {
+      await this._finishTrans();
+    }
+  }
+
+  /**
+   * Helper function to wait for all promises to finish before throwing an error.
+   * @param {Promise<any>[]} promises 
+   */
+  async promiseAll(promises) {
+    return (await Promise.allSettled(promises))
+      .map((result) => {
+        if ('rejected' == result.status) {
+          throw result.reason;
+        }
+        return result.value;
+      });
+  }
+
+  /**
+   * Triggered when a service calls its `model()` method.
+   * Use a shared database connection if a transaction exists.
+   * @param {think.Model} model 
+   */
+  _onModel(model) {
+    if (think.isEmpty(this._stack)) {
+      // No transaction
+      return;
+    }
+    const last = this._stack[this._stack.length - 1];
+    model.db(last.db());
+  }
+
+  /**
+   * Start a transaction.
+   */
+  async _startTrans() {
+    // 
+    const model = this.model(this.constructor.MODEL_NAME);
+    await model.startTrans();
+    this._stack.push(model);
+  }
+
+  /**
+   * Finish a transaction.
+   */
+  async _finishTrans() {
+    this._stack.pop();
+  }
+
+  /**
+   * Called after a transaction when no error occurred.
+   */
+  async _commit() {
+    const model = this._stack[this._stack.length - 1];
+    await model.commit();
+  }
+
+  /**
+   * Called after a transaction when an error occurred.
+   */
+  async _rollback() {
+    const model = this._stack[this._stack.length - 1];
+    await model.rollback();
+  }
+}
