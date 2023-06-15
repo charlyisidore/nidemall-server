123b160c3cba91cae138581130acf3ed05cbb923
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-15 18:05:49 +0800

    Fix: test for /wx/brand

--- test/wx/brand/list.js
+++ test/wx/brand/list.js
@@ -24,9 +24,12 @@ test.afterEach(async (t) => {
 });
 
 test('success', async (t) => {
-  const response = await request(t, {
-    ...REQUEST,
-  });
+  const response = await request(t, REQUEST);
 
   t.is(response.errno, 0);
+  t.assert(Number.isInteger(response.data.total));
+  t.assert(Number.isInteger(response.data.pages));
+  t.assert(Number.isInteger(response.data.limit));
+  t.assert(Number.isInteger(response.data.page));
+  t.assert(Array.isArray(response.data.list));
 });
