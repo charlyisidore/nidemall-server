988d729e7224d15e4b9113800dec47311437fb7d
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-21 12:08:56 +0800

    Fix: WeixinService createOrder() method might work now

--- src/common/service/weixin.js
+++ src/common/service/weixin.js
@@ -1,6 +1,7 @@
 const Base = require('./base.js');
 const axios = require('axios');
 const { XMLBuilder, XMLParser } = require('fast-xml-parser');
+const crypto = require('node:crypto');
 
 module.exports = class WeixinService extends Base {
   constructor() {
@@ -123,40 +124,76 @@ module.exports = class WeixinService extends Base {
    * 
    * @see https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_1
    * @param {object} order 
-   * @returns {Promise<{}>}
+   * @returns {Promise<{appId: string, timeStamp: string, nonceStr: string, package: string, signType: string, paySign: string}>}
    */
   async createOrder(order) {
     const config = think.config('weixin');
 
+    const tradeType = 'JSAPI';
+    const signType = 'MD5';
+    /** @type {string} */
+    const signKey = config.mchKey;
+    const nonceStr = (new Date()).getTime().toString();
+
+    const query = {
+      appid: config.appid,
+      mch_id: config.mchId,
+      notify_url: config.notifyUrl,
+      //
+      trade_type: tradeType,
+      sign_type: signType,
+      nonce_str: nonceStr,
+      //
+      out_trade_no: order.outTradeNo,
+      body: order.body,
+      total_fee: order.totalFee,
+      spbill_create_ip: order.spbillCreateIp,
+    };
+
+    Object.assign(query, {
+      sign: this.createSign(query, signType, signKey),
+    });
+
     const response = await this.request({
       method: 'post',
       url: 'https://api.mch.weixin.qq.com/pay/unifiedorder',
-      data: this.buildXml({
-        xml: {
-          appid: config.appid,
-          mch_id: config.mchId,
-          partner_key: config.partnerKey,
-          notify_url: config.notifyUrl,
-          trade_type: 'MWEB',
-          body: order.body,
-          out_trade_no: order.outTradeNo,
-          total_fee: order.totalFee,
-          spbill_create_ip: order.spbillCreateIp,
-        },
-      }),
+      data: this.buildXml({ xml: query }),
     });
 
-    const xml = this.parseXml(response)?.xml;
+    const result = this.parseXml(response)?.xml;
 
-    if (!think.isObject(xml)) {
-      throw new Error(`Weixin createOrder XML parse error`);
+    if (!think.isObject(result)) {
+      throw new Error(`weixin createOrder XML parse error`);
     }
 
-    if ('SUCCESS' != xml.return_code) {
-      throw new Error(`weixin createOrder fail: ${JSON.stringify(xml)}`);
+    if (!this.checkSign(result, signType, signKey)) {
+      think.logger.debug(`weixin createOrder checkSign 校验结果签名失败，参数：${JSON.stringify(responseXml)}`);
+      throw new Error('weixin createOrder checkSign 参数格式校验错误！');
     }
 
-    return xml;
+    if ('SUCCESS' != result.return_code) {
+      think.logger.error(`weixin createOrder fail 结果业务代码异常，返回结果：${JSON.stringify(responseXml)}`);
+      throw new Error(`weixin createOrder fail`);
+    }
+
+    if (think.isTrueEmpty(result.prepay_id)) {
+      think.logger.error(`weixin createOrder 无法获取prepay id，错误代码： '${result.err_code}'，信息：${result.err_code_des}。`)
+      throw new Error(`weixin createOrder 无法获取prepay id，错误代码： '${result.err_code}'，信息：${result.err_code_des}。`);
+    }
+
+    const now = new Date();
+
+    const data = {
+      appId: result.appid,
+      timeStamp: Math.floor(now.getTime() / 1000).toString(),
+      nonceStr: result.nonceStr,
+      package: `prepay_id=${result.prepay_id}`,
+      signType,
+    };
+
+    return Object.assign(data, {
+      paySign: this.createSign(data, signType, signKey),
+    });
   }
 
   /**
@@ -197,6 +234,30 @@ module.exports = class WeixinService extends Base {
   }
 
   /**
+   * 应用场景
+   * @see https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_5
+   * @param {{ outTradeNo: string, outRefundNo: string, totalFee: number, refundFee: number }} refundRequest
+   * @returns {Promise<object>}
+   */
+  async refund(refundRequest) {
+    const config = think.config('weixin');
+
+    const response = await this.request({
+      method: 'post',
+      url: 'https://api.mch.weixin.qq.com/secapi/pay/refund',
+      data: this.buildXml({
+        appid: config.appid,
+        secret: config.secret,
+        mch_id: config.mchId,
+        nonce_str: '',
+        sign: '',
+      }),
+    });
+
+    return response;
+  }
+
+  /**
    * 
    * @param {string} sessionKey 
    * @param {string} encryptedData 
@@ -214,6 +275,66 @@ module.exports = class WeixinService extends Base {
         timestamp: '',
       },
     };
+  }
+
+  /**
+   * 
+   * @param {object} params 
+   * @param {string} signType 'HMAC-SHA256' or 'MD5'
+   * @param {string} signKey 
+   * @param {string[]} ignoredParams 
+   * @returns {string}
+   */
+  createSign(params, signType, signKey, ignoredParams = []) {
+    ignoredParams = ignoredParams.concat([
+      'couponList',
+      'key',
+      'sign',
+      'xmlDoc',
+      'xmlString',
+    ]);
+
+    const toSign = Object.entries(params).reduce(
+      (toSign, [key, value]) => {
+        const valueStr = value.toString();
+        if ('' === valueStr || ignoredParams.includes(key)) {
+          return toSign;
+        }
+        return toSign.concat(`${key}=${valueStr}&`);
+      }
+    )
+      .concat(`key=${signKey}`);
+
+    switch (signType) {
+      case 'HMAC-SHA256':
+        return crypto
+          .createHmac('sha256', signKey)
+          .update(toSign)
+          .digest('hex')
+          .toUpperCase();
+      case 'MD5':
+        return crypto
+          .createHash('md5')
+          .update(toSign)
+          .digest('hex')
+          .toUpperCase();
+      default:
+        throw new Error(`${signType} unsupported`);
+    }
+  }
+
+  /**
+   * 
+   * @param {object} params 
+   * @param {string} signType 
+   * @param {string} signKey 
+   * @returns {boolean}
+   */
+  checkSign(params, signType, signKey) {
+    if (think.isNullOrUndefined(params.sign)) {
+      return false;
+    }
+    return params.sign == this.createSign(params, signType, signKey);
   }
 
   buildXml(obj) {
