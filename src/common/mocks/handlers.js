30aa88698b748ef3a1ddc4058861d21a5c48b3b5
Charly Lersteau <charly.lersteau@gmx.fr>
2023-06-21 18:01:45 +0800

    Add: mock for refund Weixin API

--- src/common/mocks/handlers.js
+++ src/common/mocks/handlers.js
@@ -8,7 +8,7 @@ function randomHex(n) {
   return crypto.randomBytes(n).toString('hex');
 }
 
-function randomNum(n) {
+function randomNumString(n) {
   return Math.floor(Math.random() * (Math.pow(10, n) - 1))
     .toString()
     .padStart(n, '0');
@@ -23,7 +23,7 @@ function randomPrepayId() {
   const minutes = now.getMinutes().toString().padStart(2, '0');
   const seconds = now.getSeconds().toString().padStart(2, '0');
   const hex = randomHex(5);
-  const num = randomNum(10);
+  const num = randomNumString(10);
   return `wx${year}${month}${day}${hours}${minutes}${seconds}${hex}${num}`;
 }
 
@@ -31,16 +31,25 @@ function randomNonceStr() {
   return (new Date()).getTime().toString();
 }
 
+function parseXml(text) {
+  const parser = new XMLParser();
+  return parser.parse(text);
+}
+
+function buildXml(obj) {
+  const builder = new XMLBuilder();
+  return builder.build(obj);
+}
+
 exports.handlers = [
+  // https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_1
   rest.post('https://api.mch.weixin.qq.com/pay/unifiedorder', async (req, res, ctx) => {
     /** @type {WeixinService} */
     const weixinService = think.service('weixin');
     const signKey = think.config('weixin.mchKey');
 
-    const parser = new XMLParser();
-    const query = parser.parse(await req.text())?.xml ?? {};
+    const query = parseXml(await req.text())?.xml ?? {};
 
-    console.log(query);
     const result = {
       return_code: 'SUCCESS',
       return_msg: 'OK',
@@ -56,8 +65,43 @@ exports.handlers = [
       sign: weixinService.createSign(result, query.sign_type, signKey),
     });
 
-    const builder = new XMLBuilder();
-    const body = builder.build({ xml: result });
+    const body = buildXml({ xml: result });
+
+    return res(
+      ctx.status(200),
+      ctx.xml(body)
+    );
+  }),
+
+  // https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=9_5
+  rest.post('https://api.mch.weixin.qq.com/secapi/pay/refund', async (req, res, ctx) => {
+    /** @type {WeixinService} */
+    const weixinService = think.service('weixin');
+    const signKey = think.config('weixin.mchKey');
+
+    const query = parseXml(await req.text())?.xml ?? {};
+
+    const result = {
+      appid: query.appid,
+      mch_id: query.mch_id,
+      nonce_str: randomNonceStr(),
+      out_trade_no: query.out_trade_no,
+      refund_count: '1',
+      refund_fee_0: '1',
+      refund_id_0: randomNumString(28),
+      refund_id_status: 'PROCESSING',
+      result_code: 'SUCCESS',
+      return_code: 'SUCCESS',
+      return_msg: 'OK',
+      cash_refund_fee: '90',
+      transaction_id: randomNumString(28),
+    };
+
+    Object.assign(result, {
+      sign: weixinService.createSign(result, query.sign_type, signKey),
+    });
+
+    const body = buildXml({ xml: result });
 
     return res(
       ctx.status(200),
