cdb0e0e2d30a43a12f45216a2237dc3a1b162e14
Charly Lersteau <645647+charlyisidore@users.noreply.github.com>
2023-07-11 13:57:18 +0800

    Fix: number 1 is not converted to true when logic rule is boolean

--- src/common/logic/base.js
+++ src/common/logic/base.js
@@ -4,6 +4,47 @@ module.exports = class extends think.Logic {
   }
 
   async __after() {
+    // Convert data before validation
+    Object.entries(this.rules)
+      .forEach(([argName, rule]) => {
+        const method = this._getCtxMethod(rule.method);
+        const value = this.ctx[method](argName);
+
+        if (think.isUndefined(value)) {
+          return;
+        }
+
+        // ThinkJS converts the string '1' to true, but not the number 1
+        // https://github.com/thinkjs/think-validator/blob/846bd2caec7cb06c41fd05bf5f8082e90e928eaf/src/index.js#L299
+        if (rule.boolean && 1 === value) {
+          this.ctx[method](argName, true);
+        }
+      });
+
     return super.__after?.();
+  }
+
+  _getCtxMethod(method) {
+    // https://github.com/thinkjs/think-validator/blob/846bd2caec7cb06c41fd05bf5f8082e90e928eaf/src/index.js#L239
+    const httpMethod = (think.isUndefined(method) || '' === method)
+      ? this.ctx.method.toUpperCase()
+      : method.toUpperCase();
+
+    // https://github.com/thinkjs/think-validator/blob/a44f08ee7407db9a7e0c2f3c82cf602f710f36b2/src/method.js
+    switch (httpMethod) {
+      case 'GET':
+        return 'param';
+      case 'POST':
+      case 'PUT':
+      case 'DELETE':
+      case 'PATCH':
+      case 'LINK':
+      case 'UNLINK':
+        return 'post';
+      case 'FILE':
+        return 'file';
+      default:
+        return 'param';
+    }
   }
 };
