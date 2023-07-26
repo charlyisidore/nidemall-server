e48f6a516b83d7d01402e2d666b252c9562265fb
Charly Lersteau <645647+charlyisidore@users.noreply.github.com>
2023-07-26 15:54:25 +0800

    Add: /wx/user/index unit test

---
+++ test/wx/user/index.js
@@ -0,0 +1,36 @@
+const test = require('ava');
+const { request, createUser, destroyUser } = require('../../helpers/app.js');
+const { validateResponse } = require('../../helpers/openapi.js');
+
+const REQUEST = {
+  method: 'get',
+  path: '/wx/user/index',
+};
+
+// Create a user
+test.before(async (t) => {
+  const { token, id } = await createUser();
+  t.context.token = token;
+  t.context.userId = id;
+});
+
+// Delete the user
+test.after.always(async (t) => {
+  await destroyUser(t.context.userId);
+});
+
+test('success', async (t) => {
+  const response = await request(t, {
+    ...REQUEST,
+    token: t.context.token,
+  });
+
+  t.is(response.errno, 0);
+  await t.notThrowsAsync(() => validateResponse(REQUEST, response));
+});
+
+test('not logged in', async (t) => {
+  const response = await request(t, REQUEST);
+
+  t.is(response.errno, 501);
+});
