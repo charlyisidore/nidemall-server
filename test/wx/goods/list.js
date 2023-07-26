13ff8f17b79cf9ab2376369410e969fcf9bf7e5b
Charly Lersteau <645647+charlyisidore@users.noreply.github.com>
2023-07-26 16:11:56 +0800

    Add: unit test for /wx/goods/list

---
+++ test/wx/goods/list.js
@@ -0,0 +1,51 @@
+const test = require('ava');
+const { request } = require('../../helpers/app.js');
+const { createCategory, destroyCategory } = require('../helpers/category.js');
+const { createGoods, destroyGoods } = require('../helpers/goods.js');
+const { validateResponse } = require('../../helpers/openapi.js');
+
+const REQUEST = {
+  method: 'get',
+  path: '/wx/goods/list',
+};
+
+test('success', async (t) => {
+  const goodsList = await Promise.all(
+    Array.from({ length: 11 }, () => createGoods())
+  );
+
+  t.teardown(async () => {
+    await Promise.all(goodsList.map((goods) => destroyGoods(goods.id)));
+  });
+
+  const response = await request(t, REQUEST);
+
+  t.is(response.errno, 0);
+  await t.notThrowsAsync(() => validateResponse(REQUEST, response));
+});
+
+
+test('with category id', async (t) => {
+  const category = await createCategory();
+  const goodsList = await Promise.all(
+    Array.from(
+      { length: 11 },
+      () => createGoods({
+        categoryId: category.id,
+      })
+    )
+  );
+
+  t.teardown(async () => {
+    await Promise.all(goodsList.map((goods) => destroyGoods(goods.id)));
+    await destroyCategory(category.id);
+  });
+
+  const response = await request(t, {
+    ...REQUEST,
+    data: { id: category.id },
+  });
+
+  t.is(response.errno, 0);
+  await t.notThrowsAsync(() => validateResponse(REQUEST, response));
+});
